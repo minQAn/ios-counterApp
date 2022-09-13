4e183f331136a1b8f4628cb82c916dc48ccae345
minQ <minq.an7@gmail.com>
2022-09-13 15:42:39 -0400

    Initial Commit

---
+++ counter_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  counter_app
+//
+//  Created by 안민규 on 2022-09-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ counter_app/counter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  counter_appApp.swift
+//  counter_app
+//
+//  Created by 안민규 on 2022-09-13.
+//
+
+import SwiftUI
+
+@main
+struct counter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
